ba780decf78789267031e2181845ba0bcf611da2
zeuw22 <dsanhur.18@iesmarquesdecomares.org>
2026-01-16 10:00:43 +0100

    Update storage zeuw.py

--- storage zeuw.py
+++ storage zeuw.py
@@ -1,3 +1,20 @@
+# storage.py
+# =================================================
+# Este archivo gestiona el guardado y la carga
+# de la lista de tareas en un archivo JSON.
+#
+# Funciones a implementar:
+#   - guardar_tareas(tareas, archivo="tareas.json")
+#   - cargar_tareas(archivo="tareas.json")hola ajdjasdja
+#
+# PISTA:
+#   Usa json.dump() para guardar
+#   Usa json.load() para cargar
+#
+# Si el archivo no existe al cargar, deberías devolver una lista vacía.
+# =================================================ç
+
+
 import json
 
 def guardar_tareas(tareas, archivo="tareas.json"):
